986c0b4445a986dae255d4fe84b24eb70bf380d1
hckang80 <hckang80@gmail.com>
2021-12-20 00:20:32 +0900

    feat: Add mock default layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+useMeta({
+  title: 'Nuxt3 Boilerplate',
+  htmlAttrs: {
+    lang: 'en',
+  },
+})
+
+</script>
+
+<template>
+  <div>
+    Some shared layout content:
+    <slot />
+  </div>
+</template>
